0de9f287e136bf23aca3509cf3cd3929438eef57
Cassandra King <puddincat@gmail.com>
2022-01-17 19:48:10 -0500

    update with argon2 encrypting lib and update resolvers

---
+++ LIREDDIT-SERVER/src/resolvers/user.ts
@@ -0,0 +1,32 @@
+import { MyContext } from './../types';
+import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { User } from 'src/entities/User';
+import argon2 from 'argon2';
+
+// Inputs are an alternate way to Args in GraphQL
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string
+  @Field()
+  password: string
+}
+
+
+@Resolver()
+export class UserResolver {
+  // craete class and decorate with @Resolver decorator
+  @Mutation(() => String)
+  async register(
+    @Arg('userOptions') userOptions: UsernamePasswordInput,
+    @Ctx() {em}: MyContext
+  ) {
+    const hashedPassword = await argon2.hash(userOptions.password)// since it returns a promise, await it
+    const user =  em.create(User, {
+      username: userOptions.username,
+      password: hashedPassword // we save this in DB
+    })// do not want to pass the user pw
+    await em.persistAndFlush(user) //saves user to DB do not want to use plain text version of pw either we will encrpty with argon2
+    return user
+  }
+}
